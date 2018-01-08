b6c5eb1cace27170c0891fceee40f2d939ae2140
noakes <old.noakes@gmail.com>
2018-01-08 17:10:39 +1100

    Legacy naming of running docker instance

--- tests/inventories/inventory_docker.sh
+++ tests/inventories/inventory_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DOCKER_HOST=127.0.0.1
-DOCKER_PORT=`docker port ANSIBLE_ROLE_CHANGE_THIS-centos7 22 | awk -F: '{ print $2 }'`
+DOCKER_PORT=`docker port ANSIBLE_ROLE_CHANGE_THIS 22 | awk -F: '{ print $2 }'`
 
 echo "{\"all\":{\"hosts\":[\"${DOCKER_HOST}\"],\"vars\":{\"ansible_ssh_port\":\"${DOCKER_PORT}\"}}}"
