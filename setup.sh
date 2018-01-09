519fb4278a9e094b37d2d0e2029ecf5d7119fe20
noakes <old.noakes@gmail.com>
2018-01-09 16:25:30 +1100

    Setting up a default value for the role replacement in the setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 
 which gsed 1>/dev/null || { echo "Please install gsed via homebrew since osx sed sucks"; exit 1; }
 
-echo "Please provide the role name: "
+DEFAULT_ROLE=$(basename $PWD)
+printf "Please provide the role name (Default: ${DEFAULT_ROLE}): "
 read ROLE
+ROLE=${ROLE:-$DEFAULT_ROLE}
 echo "Replacing ANSIBLE_ROLE_CHANGE_THIS with $ROLE in all files"
+exit 1
 find . -type f ! -name setup.sh -exec gsed -i -e 's/ANSIBLE_ROLE_CHANGE_THIS/'"$ROLE"'/g' {} \;
 
 echo "Cleanup time:"
