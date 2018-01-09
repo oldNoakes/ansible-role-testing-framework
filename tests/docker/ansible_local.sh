9bfcdbfc9f01ca7cdbf65207707b5d202c7d90c5
noakes <old.noakes@gmail.com>
2018-01-09 11:19:30 +1100

    Renaming inventory files to remove duplicated info

--- tests/docker/ansible_local.sh
+++ tests/docker/ansible_local.sh
@@ -2,4 +2,4 @@
 
 TEST_DIR="$(pwd)/tests"
 echo "Running ansible playbook from: ${TEST_DIR}"
-ansible-playbook --inventory ${TEST_DIR}/inventories/inventory_docker.sh --key-file /var/tmp/docker_ssh/id_rsa ${TEST_DIR}/test.yml
+ansible-playbook --inventory ${TEST_DIR}/inventory/docker.sh --key-file /var/tmp/docker_ssh/id_rsa ${TEST_DIR}/test.yml
